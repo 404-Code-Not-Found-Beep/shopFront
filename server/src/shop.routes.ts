2af5cfa3668667c6f877976356a078fe37f4a60e
Jake <jameskcamilleri@gmail.com>
2022-12-13 17:53:47 +0000

    svr

--- server/src/shop.routes.ts
+++ server/src/shop.routes.ts
@@ -34,7 +34,7 @@ shopRouter.get('/shirts', async (req,res) => {
         res.status(500).send(error.message);
     }
 })
-
+//this could be used with the search bar fnctionality
 // shopRouter.get('/:id', async (req,res) => {
 //     try {
 //         const id = req?.params?.id;
@@ -50,19 +50,3 @@ shopRouter.get('/shirts', async (req,res) => {
 //         res.status(404).send(`Failed to find an item: ID ${req?.params?.id}`);
 //     }
 // });
-
-// shopRouter.post('/', async (req, res) => {
-//     try {
-//         const item = req.body;
-//         const result = await collections.items.insertOne(item);
-  
-//         if (result.acknowledged) {
-//             res.status(201).send(`Created a new item: ID ${result.insertedId}.`);
-//         } else {
-//             res.status(500).send("Failed to create a new item.");
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(400).send(error.message);
-//     }
-//  });
